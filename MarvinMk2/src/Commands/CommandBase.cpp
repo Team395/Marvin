d805527f0b16e74937fac5234f2e1a0dd7764750
HPrickettMorgan <hprickett-morgan2019@columbiasecondary.org>
2018-01-19 15:23:38 -0500

    Added CommandBase

---
+++ MarvinMk2/src/Commands/CommandBase.cpp
@@ -0,0 +1,20 @@
+/*
+ * CommandBase.cpp
+ *
+ *  Created on: Jan 19, 2018
+ *      Author: JARVIS
+ */
+
+#include <Commands/CommandBase.h>
+#include <string>
+CommandBase::CommandBase(std::string name) : name{name} {
+
+}
+
+CommandBase::~CommandBase() {
+	// TODO Auto-generated destructor stub
+}
+
+std::string CommandBase::getName(){
+	return name;
+}
---
+++ MarvinMk2/src/Commands/CommandBase.h
@@ -0,0 +1,27 @@
+/*
+ * CommandBase.h
+ *
+ *  Created on: Jan 19, 2018
+ *      Author: JARVIS
+ */
+
+#ifndef SRC_COMMANDS_COMMANDBASE_H_
+#define SRC_COMMANDS_COMMANDBASE_H_
+#include <string>
+
+class CommandBase {
+	std::string name;
+
+public:
+	CommandBase(std::string);
+	virtual ~CommandBase();
+
+	virtual void init()=0;
+	virtual void update()=0;
+	virtual void finished()=0;
+	virtual bool isFinished()=0;
+
+	std::string getName();
+};
+
+#endif /* SRC_COMMANDS_COMMANDBASE_H_ */
