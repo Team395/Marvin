1c759a59f9d0e62630b77224873b2011611c5847
HPrickettMorgan <hprickett-morgan2019@columbiasecondary.org>
2018-01-19 17:31:09 -0500

    Added a list of all constructed subsystems in SubsystemBase

--- MarvinMk2/src/Systems/SystemBase.cpp
+++ MarvinMk2/src/Systems/SystemBase.cpp
@@ -4,15 +4,28 @@
  *  Created on: Jan 19, 2018
  *      Author: JARVIS
  */
-
+#include <cassert>
 #include "SystemBase.h"
 
 SystemBase::SystemBase(std::string name) : name{name} {
-	// TODO Auto-generated constructor stub
-
+	//Makes sure system is unique
+	for(auto iter = systemsList.begin(); iter != systemsList.end(); iter++){
+		assert(name != (*iter)->name);
+	}
+	//If this passes, adds the currently initializing system to the list
+	systemsList.push_back(this);
 }
 
 SystemBase::~SystemBase() {
-	// TODO Auto-generated destructor stub
+	//If this destructor is ever called (not sure why it would be) a system should be removed from the systems list
+	for(auto iter = systemsList.begin(); iter != systemsList.end(); iter++){
+		if(*iter == this){
+			systemsList.erase(iter);
+		}
+	}
+}
+
+static const std::vector<SystemBase*>& SystemBase::getSystemsList() {
+	return systemsList;
 }
 
--- MarvinMk2/src/Systems/SystemBase.h
+++ MarvinMk2/src/Systems/SystemBase.h
@@ -8,13 +8,17 @@
 #ifndef SRC_SYSTEMS_SYSTEMBASE_H_
 #define SRC_SYSTEMS_SYSTEMBASE_H_
 #include <string>
+#include <vector>
 
 class SystemBase {
+	static std::vector<SystemBase*> systemsList{};
 	std::string name;
 
 public:
 	SystemBase(std::string);
 	virtual ~SystemBase();
+
+	static const std::vector<SystemBase*>& getSystemsList();
 };
 
 #endif /* SRC_SYSTEMS_SYSTEMBASE_H_ */
