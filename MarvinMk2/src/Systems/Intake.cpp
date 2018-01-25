873dfc87685b0d6335fffe06af2d7d5d204ba485
HPrickettMorgan <hprickett-morgan2019@columbiasecondary.org>
2018-01-24 23:50:43 -0500

    Create intake system

---
+++ MarvinMk2/src/Systems/Intake.cpp
@@ -0,0 +1,18 @@
+/*
+ * Intake.cpp
+ *
+ *  Created on: Jan 24, 2018
+ *      Author: hlewi
+ */
+
+#include <Systems/Intake.h>
+
+Intake::Intake() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Intake::~Intake() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ MarvinMk2/src/Systems/Intake.h
@@ -0,0 +1,20 @@
+/*
+ * Intake.h
+ *
+ *  Created on: Jan 24, 2018
+ *      Author: hlewi
+ */
+
+#ifndef SRC_SYSTEMS_INTAKE_H_
+#define SRC_SYSTEMS_INTAKE_H_
+#include <Talon.h>
+
+#include <Systems/SystemBase.h>
+
+class Intake {
+public : SystemBase:
+	Intake() ;
+	virtual ~Intake();
+};
+
+#endif /* SRC_SYSTEMS_INTAKE_H_ */
