e78b4c23bd124bab3e00ddc51bb087b3a2650833
Henry Prickett-Morgan <hprickett-morgan2019@columbiasecondary.org>
2018-03-11 19:10:48 -0400

    add DriveFeetCommand

--- MarvinMk2/src/Commands/DriveFeetCommand.cpp
+++ MarvinMk2/src/Commands/DriveFeetCommand.cpp
@@ -6,13 +6,64 @@
  */
 
 #include <Commands/DriveFeetCommand.h>
+#include <SmartDashboard/SmartDashboard.h>
 
-Drive__FeetCommand::Drive__FeetCommand(double feet) : CommandBase("Drive %d Feet Command") {
+
+Drive__FeetCommand::Drive__FeetCommand(double feet, Drivebase* drivebase, DrivebaseEncoderSensors* encoderSensors) :
+	CommandBase("Drive Feet Command"),
+	pidController{encoderSensors->kP, encoderSensors->kI,encoderSensors->kD, encoderSensors, drivebase},
+	encoderSensors{encoderSensors},
+	requestedMovementFeet{feet}
+	{
 	// TODO Auto-generated constructor stub
 
 }
 
 Drive__FeetCommand::~Drive__FeetCommand() {
 	// TODO Auto-generated destructor stub
+
 }
 
+void Drive__FeetCommand::init(){
+   CommandBase::init();
+}
+
+void Drive__FeetCommand::update(){
+	encoderSensors->kP = encoderSensors->preferences->GetDouble("DriveFeetKp", -.03);
+	encoderSensors->kI = encoderSensors->preferences->GetDouble("DriveFeetKi", 0);
+	encoderSensors->kD = encoderSensors->preferences->GetDouble("DriveFeetKd", 0);
+
+	pidController.SetP(encoderSensors->kP);
+	pidController.SetI(encoderSensors->kI);
+	pidController.SetD(encoderSensors->kD);
+	encoderSensors->setMinimumPidOutput(encoderSensors->preferences->GetDouble("Drive Feet Minimum PID Output", 0));
+
+	if(!pidController.IsEnabled() && !movementFinished){
+		encoderSensors->resetEncoderSensors();
+		pidController.SetSetpoint(encoderSensors->getAveragedEncoderPositions() + encoderSensors->convertToNativeUnits(requestedMovementFeet));
+		pidController.SetAbsoluteTolerance(kAcceptableError);
+		pidController.Enable();
+	}
+
+	if(pidController.OnTarget()){
+		movementFinished = true;
+		pidController.Disable();
+	}
+	frc::SmartDashboard::PutData("Drive Feet PID Controller", &pidController);
+	frc::SmartDashboard::PutBoolean("Drive Feet Finished", pidController.OnTarget());
+	frc::SmartDashboard::PutNumber("Drive Feet Average Encoder Positions", encoderSensors->getAveragedEncoderPositions());
+	frc::SmartDashboard::PutNumber("Drive Feet PIDError", pidController.GetError());
+	frc::SmartDashboard::PutNumber("Drive Feet PIDSetpoint", pidController.GetSetpoint());
+
+}
+void Drive__FeetCommand::finish(){
+	CommandBase::finish();
+}
+
+void Drive__FeetCommand::disable(){
+	pidController.Disable();
+	frc::SmartDashboard::PutData("Drive Feet PID Controller", &pidController);
+}
+void Drive__FeetCommand::startNewMovement(){
+	movementFinished = false;
+}
--- MarvinMk2/src/Commands/DriveFeetCommand.h
+++ MarvinMk2/src/Commands/DriveFeetCommand.h
@@ -9,11 +9,25 @@
 #define SRC_COMMANDS_DRIVEFEETCOMMAND_H_
 
 #include <Commands/CommandBase.h>
+#include <PIDController.h>
+#include <Systems/DrivebaseEncoderSensors.h>
 
 class Drive__FeetCommand: public CommandBase {
+	PIDController pidController;
+	DrivebaseEncoderSensors* encoderSensors;
+	bool movementFinished = true;
+	double requestedMovementFeet = 0;
+	double kAcceptableError=651.9; //actually two inches
 public:
-	Drive__FeetCommand(double);
+	Drive__FeetCommand(double, Drivebase* drivebase, DrivebaseEncoderSensors* encoderSensors);
 	virtual ~Drive__FeetCommand();
+
+	void init() override;
+	void update() override;
+	void finish() override;
+
+	void disable();
+	void startNewMovement();
 };
 
 #endif /* SRC_COMMANDS_DRIVEFEETCOMMAND_H_ */
